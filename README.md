df9e6af2bdab9ea0c619a275d0771a79bda9dd54
itcloudy <272685110@qq.com>
2018-12-18 11:21:47 +0800

    cjapter-2

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # chat_app
 
+* [Dart Flutter Chat App Tutorial](https://github.com/itcloudy/chat_app/tree/chapter-1)
+* [Making Http requests and Using Json in Dart's Flutter Framework](https://github.com/itcloudy/chat_app/tree/chapter-2)
--- lib/main.dart
+++ lib/main.dart
@@ -1,172 +1,89 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-final ThemeData iOSTheme = new ThemeData(
-  primarySwatch: Colors.red,
-  primaryColor: Colors.grey[400],
-  primaryColorBrightness: Brightness.dark,
-);
-final ThemeData androidTheme = new ThemeData(
-  primarySwatch: Colors.blue[800],
-  accentColor: Colors.green[100],
-);
-const String defaultUserName = "何 Doe";
+void main() => runApp(MaterialApp(
+  home: StarWarsData(),
+));
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
+class StarWarsData extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: "Chat Appplication",
-      theme:
-          defaultTargetPlatform == TargetPlatform.iOS ? iOSTheme : androidTheme,
-      home: new Chat(),
-    );
+  _StarWarsDataState createState() => _StarWarsDataState();
+}
+
+class _StarWarsDataState extends State<StarWarsData> {
+  final String url = "https://swapi.co/api/starships";
+  List data;
+
+  Future<String> getSWData() async {
+    var res = await http
+        .get(Uri.encodeFull(url),headers: {"Accept":"application/json"});
+    setState(() {
+      var resBody = json.decode(res.body);
+      data= resBody['results'];
+    });
+    return "Success!";
   }
-}
-
-class Chat extends StatefulWidget {
-  @override
-  State createState() => ChatWindow();
-}
-
-class ChatWindow extends State<Chat> with TickerProviderStateMixin {
-  final List<Msg> _message = <Msg>[];
-  final TextEditingController _textController = new TextEditingController();
-  bool _isWriting = false;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: new AppBar(
-        title: new Text("Chat Application"),
-        elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 6.0,
+      appBar: AppBar(
+        title: Text("Star Wars Starships"),
+        backgroundColor: Colors.amberAccent,
       ),
-      body: new Column(children: <Widget>[
-        new Flexible(
-            child: new ListView.builder(
-              itemBuilder: (_,int index)=>_message[index],
-              itemCount: _message.length,
-              reverse: true,
-              padding: new EdgeInsets.all(6.0),
-            )),
-        new Divider(height: 1.0),
-        new Container(
-          child: _buildComposer(),
-          decoration: new BoxDecoration(color: Theme.of(context).cardColor),
-        ),
-      ],),
-    );
-  }
-  Widget _buildComposer(){
-    return new IconTheme(
-        data: new IconThemeData(color: Theme.of(context).accentColor),
-        child: new Container(
-          margin: const EdgeInsets.symmetric(horizontal: 9.0),
-          child: new Row(
-            children: <Widget>[
-              new Flexible(
-                  child: new TextField(
-                    controller: _textController,
-                    onChanged: (String txt){
-                      setState(() {
-                        _isWriting = txt.length > 0;
-                      });
-                    },
-                    onSubmitted: _submitMsg,
-                    decoration: 
-                     new InputDecoration.collapsed(hintText: "Enter some text to send a message"),
-                  ),
-              ),
-              new Container(
-                margin: new EdgeInsets.symmetric(horizontal: 3.0),
-                child: Theme.of(context).platform == TargetPlatform.iOS
-                ? new CupertinoButton(
-                    child: new Text("Submit"),
-                    onPressed: _isWriting?()=> _submitMsg(_textController.text):null)
-                : new IconButton(
-                    icon: new Icon(Icons.message),
-                    onPressed: _isWriting
-                    ? () => _submitMsg(_textController.text):null,
-                ),
-              ),
-            ],
-          ),
-          decoration: Theme.of(context).platform == TargetPlatform.iOS
-          ? new BoxDecoration(
-            border:
-              new Border(top: new BorderSide(color: Colors.brown[200]))):
-          null
-        ),
-    );
-  }
-  void _submitMsg(String txt){
-    _textController.clear();
-    setState(() {
-      _isWriting = false;
-    });
-    Msg msg = new Msg(
-      txt: txt,
-      animationController: new AnimationController(
-          vsync: this,
-        duration: new Duration(microseconds: 800)
-      ),
-    );
-    setState(() {
-      _message.insert(0, msg);
-    });
-    msg.animationController.forward();
-  }
-
-  @override
-  void dispose() {
-    for (Msg msg in _message){
-      msg.animationController.dispose();
-    }
-    super.dispose();
-  }
-
-}
-
-class Msg extends StatelessWidget {
-  Msg({this.txt,this.animationController});
-  final String txt;
-  final AnimationController animationController;
-
-  @override
-  Widget build(BuildContext context) {
-    return new SizeTransition(sizeFactor: new CurvedAnimation(
-        parent: animationController,
-        curve: Curves.easeOut
-    ),
-      axisAlignment: 0.0,
-      child: new Container(
-        margin: const EdgeInsets.symmetric(vertical: 8.0),
-        child: new Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            new Container(
-              margin: const EdgeInsets.only(right: 10.0),
-              child: new CircleAvatar(child: new Text(defaultUserName[0])),
-            ),
-            new Expanded(
-                child: new Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+      body: ListView.builder(
+        itemCount: data==null ? 0 : data.length,
+          itemBuilder: (BuildContext context,int index){
+            return  new Container(
+              child: Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: <Widget>[
-                    new Text(defaultUserName,style: Theme.of(context).textTheme.subhead),
-                    new Container(
-                      margin: const EdgeInsets.only(top: 6.0),
-                      child: new Text(txt),
+                    Card(
+                      child: Container(
+                        padding: EdgeInsets.all(15.0),
+                        child: Row(
+                          children: <Widget>[
+                            Text("Name: "),
+                            Text(data[index]["name"],
+                            style: TextStyle(
+                              fontSize: 18.0,color: Colors.black54
+                            )),
+                          ],
+                        ),
+
+                      ),
                     ),
+                    Card(
+                      child: Container(
+                        padding: EdgeInsets.all(15.0),
+                        child: Row(
+                          children: <Widget>[
+                            Text("Model: "),
+                            Text(data[index]["model"],
+                                style: TextStyle(
+                                    fontSize: 18.0,color: Colors.red
+                                )),
+                          ],
+                        ),
+                      ),
+                    )
                   ],
                 ),
-            ),
-          ],
-        ),
+              ),
+            );
+          }
       ),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    this.getSWData();
   }
 
 }
