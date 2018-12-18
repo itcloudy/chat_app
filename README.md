9a8dec13ff07b2eec603fe6435ac806b5c575cd9
itcloudy <272685110@qq.com>
2018-12-18 12:53:41 +0800

    Building Dynamic Lists with Streams in Dart's Flutter Framework

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # chat_app
-
+[](https://github.com/itcloudy/chat_app/tree/chapter-)
 * [Dart Flutter Chat App Tutorial](https://github.com/itcloudy/chat_app/tree/chapter-1)
 * [Building a Multi-Page Application with Dart's Flutter Mobile Framework](https://github.com/itcloudy/chat_app/tree/chapter-2)
 * [Making Http requests and Using Json in Dart's Flutter Framework](https://github.com/itcloudy/chat_app/tree/chapter-3)
+* [Building Dynamic Lists with Streams in Dart's Flutter Framework](https://github.com/itcloudy/chat_app/tree/chapter-4)
--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,103 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-
-
-final ThemeData themeData = ThemeData(
-  canvasColor: Colors.lightGreenAccent,
-);
-
-void main() => runApp(MaterialApp(
-  home: MyApp(),
-  theme: themeData,
-));
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Photo Streamer",
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: PhotoList(),
+    );
+  }
+}
+class PhotoList extends StatefulWidget {
+  @override
+  _PhotoListState createState() => _PhotoListState();
+
+}
+
+class _PhotoListState extends State<PhotoList> {
+  StreamController<Photo> streamController;
+  List<Photo> list =[];
+
+  @override
+  void initState() {
+    super.initState();
+    streamController = StreamController.broadcast();
+    streamController.stream.listen((p)=>
+        setState(()=>list.add(p)
+        ));
+    load(streamController);
+  }
+
+  load(StreamController sc) async{
+    String url = "https://jsonplaceholder.typicode.com/photos";
+    var client = new http.Client();
+    var req = new http.Request('get', Uri.parse(url));
+    var streamedRes = await client.send(req); 
+    streamedRes.stream
+    .transform(utf8.decoder)
+    .transform(json.decoder)
+    .expand((e)=>e)
+    .map((map)=>Photo.fromJsonMap(map))
+    .pipe(streamController);
+
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    streamController?.close();
+    streamController = null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Photo Streams"),
+      ),
       body: Center(
-        child: FlatButton(
-            onPressed: (){
-              Navigator.push(context, PageTwo());
-            },
-            child: Text("Go to Page Two"),
+        child: ListView.builder(
+
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (BuildContext context,int index)=> _makeElement(index),
+        ),
+      ),
+    );
+
+  }
+  Widget _makeElement(int index){
+    if (index >= list.length){
+      return null;
+    }
+    return Container(
+      padding: EdgeInsets.all(5.0),
+
+      child: Padding(
+        padding: EdgeInsets.only(top: 200.0),
+        child: Column(
+          children: <Widget>[
+            Image.network(list[index].url,width:150.0,height: 150.0,),
+            Text(list[index].title),
+          ],
         ),
       ),
     );
   }
 }
 
-class PageTwo extends MaterialPageRoute<Null>{
-  PageTwo(): super(builder:(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).canvasColor,
-        elevation: 1.0,
-      ),
-      body: Center(
-        child: RaisedButton(
-            onPressed: (){
-            Navigator.push(context, PageThree());
-        },
-            child: Text("Go to Page Three"),
-        ),
-      ),
-    );
-  });
-}
 
-class PageThree extends MaterialPageRoute<Null>{
-  PageThree():super(builder:(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Last Page"),
-        backgroundColor: Theme.of(context).accentColor,
-        elevation: 2.0,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.close),
-            onPressed: (){
-              Navigator.pop(context);
-            },
-          ),
-        ],
-      ),
-      body: Center(
-        child: MaterialButton(
-            onPressed: (){
-              Navigator.popUntil(context, ModalRoute.withName(Navigator.defaultRouteName));
-            },
-          child: Text("Go Home"),
-            ),
-      ),
-    );
-  });
+class Photo {
+  final String title;
+  final String url;
+  Photo.fromJsonMap(Map map):title =map['title'],url =map['url'];
 }
