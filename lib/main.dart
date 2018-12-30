570338e9c83252bb7bbb80fa1c3feaff657fc328
itcloudy <272685110@qq.com>
2018-12-30 22:09:45 +0800

    base widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,163 +2,59 @@
 // Use of this source code is governed by a MIT style
 // license that can be found in the LICENSE file.
 import 'package:flutter/material.dart';
-import 'dart:math' as math;
+import 'input.dart';
+import 'package:chat_app/form.dart';
 
 
-void main() => runApp( MaterialApp(
-  home: MyApp(),
-  theme: ThemeData(
-    canvasColor: Colors.blueGrey,
-    iconTheme: IconThemeData(
-      color: Colors.white,
-    ),
-    accentColor: Colors.pinkAccent,
-    brightness: Brightness.dark,
-  ),
-));
+void main() => runApp(MyApp());
 
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp>  with TickerProviderStateMixin{
-  AnimationController controller;
-  String get timerString{
-    Duration duration = controller.duration * controller.value;
-    return '${duration.inMinutes}:${(duration.inSeconds % 60).toString().padLeft(2,'0')}';
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    controller = AnimationController(
-      vsync: this,
-      duration: Duration(seconds: 20),
-    );
-  }
+class MyApp extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    ThemeData themeData = Theme.of(context);
-    return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(8.0),
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              child: Align(
-                alignment: FractionalOffset.center,
-                child: AspectRatio(
-                  aspectRatio: 1.0,
-                  child: Stack(
-                    children: <Widget>[
-                      Positioned.fill(
-                        child: AnimatedBuilder(
-                            animation: controller,
-                            builder: (BuildContext context,Widget child){ 
-                              return new CustomPaint(
-                                painter: TimerPainter(
-                                  animation: controller,
-                                  backgroundColor: Colors.white,
-                                  color: themeData.indicatorColor,
-                                ),
-                              );
-                            }),
-                      ),
-                      Align(
-                        alignment: FractionalOffset.center,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: <Widget>[
-                            Text(
-                              "Count Down",
-                              style: themeData.textTheme.subhead,
-                            ),
-                            AnimatedBuilder(
-                              animation: controller,
-                              builder: (BuildContext context,Widget child){
-                                return new Text(
-                                  timerString,
-                                  style: themeData.textTheme.display4,
-                                );
-                              },
-
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  FloatingActionButton(
-                    child: AnimatedBuilder(
-                        animation: controller,
-                        builder: (BuildContext context ,Widget child){
-                          return new Icon(controller.isAnimating ? Icons.pause:Icons.play_arrow);
-                        },
-                    ),
-                    onPressed: (){
-                      if (controller.isAnimating){
-                        controller.stop();
-                      }else{
-                        controller.reverse(
-                          from: controller.value == 0.0 ? 1.0 :controller.value
-                        );
-                      }
-                    },
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+    return Container(
+      decoration: BoxDecoration(color: Colors.white),
+      child: MaterialApp(
+        title: "test",
+        color: Colors.blue,
+        home: AllPage(),
       ),
     );
   }
-
 }
 
-class TimerPainter extends CustomPainter{
-  final Animation<double> animation;
-  final Color backgroundColor, color;
-
-  TimerPainter({
-    this.animation,
-    this.backgroundColor,
-    this.color,
-  }) :super(repaint:animation);
-
+class AllPage extends StatelessWidget {
   @override
-  void paint(Canvas canvas, Size size) {
-    Paint paint = Paint()
-        ..color =backgroundColor
-        ..strokeWidth = 5.0
-        ..strokeCap = StrokeCap.round
-        ..style =PaintingStyle.stroke;
-
-    canvas.drawCircle(size.center(Offset.zero), size.width / 2.0, paint);
-    paint.color = color;
-    double progress = (1.0 - animation.value) * 2 * math.pi;
-    canvas.drawArc(Offset.zero & size, math.pi *1.5 , -progress, false, paint);
-
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        FlatButton(
+          child: Text("输入框"),
+          textColor: Colors.blue,
+          onPressed: (){
+            Navigator.push(context,
+              new MaterialPageRoute(
+                builder: (context){
+                  return new Input();
+                }
+              ),
+            );
+          },
+        ),
+        FlatButton(
+          child: Text("表单"),
+          textColor: Colors.blue,
+          onPressed: (){
+            Navigator.push(context,
+              new MaterialPageRoute(
+                  builder: (context){
+                    return new FormTest();
+                  }
+              ),
+            );
+          },
+        ),
+      ],
+    );
   }
-
-  @override
-  bool shouldRepaint(TimerPainter old) {
-    return animation.value != old.animation.value ||
-    color != old.color ||
-    backgroundColor != old.backgroundColor;
-  }
-
-
 }
