9fb9a2daa5b6be812a7e091bc3f3e385cbef0acb
itcloudy <272685110@qq.com>
2018-12-31 21:21:50 +0800

    use url_launcher

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,10 @@
 // Use of this source code is governed by a MIT style
 // license that can be found in the LICENSE file.
 import 'package:flutter/material.dart';
-import 'dart:io';
-import 'package:path_provider/path_provider.dart';
 import 'dart:async';
-import 'package:flutter/foundation.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-
+const URL = "https://www.youtube.com";
 
 void main() => runApp(MyApp());
 
@@ -16,122 +14,46 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Reading and Writing to Storage",
-      home: Home(
-        storage: Storage(),
-      ),
+      title: "Webview Example",
+      theme: ThemeData.dark(),
+      home: Home(),
     );
   }
 }
 class Home extends StatefulWidget {
-  final Storage storage;
-
-  Home({Key key, @required this.storage}):super(key:key);
-
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-  TextEditingController controller = TextEditingController();
-  String state;
-  Future<Directory> _appDocDir;
-
-
-  @override
-  void initState() {
-    super.initState();
-    widget.storage.readData().then((String value){
-      setState(() {
-        state = value;
-      });
-    });
-  }
-
-  Future<File> writeData() async{
-    setState(() {
-      state = controller.text;
-      controller.text = '';
-
-    });
-    return widget.storage.writeData(state);
-  }
-
-  void getAppDirectory(){
-    setState(() {
-      _appDocDir = getApplicationDocumentsDirectory();
-    });
+  Future launchURL(String url) async{
+    if (await canLaunch(url)){
+      await launch(url,forceSafariVC: true,forceWebView: true);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold (
+    return Scaffold(
       appBar: AppBar(
-        title: Text(("Reading and Writing Files")
-        ),
+        title: Text("Webview"),
       ),
-      body: Center(
+      body:Center(
         child: Column(
           children: <Widget>[
-            Text('${state ?? "File is Empty"}'),
-            TextField(
-              controller: controller,
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: Text(URL),
             ),
             RaisedButton(
-              onPressed: writeData,
-              child: Text('Write to File'),
-            ),
-            RaisedButton(
-              child: Text('Get DIR path'),
-              onPressed: getAppDirectory,
-            ),
-            FutureBuilder<Directory>(
-              future: _appDocDir,
-              builder: (BuildContext context,AsyncSnapshot<Directory> snaps ){
-                Text text = Text("");
-                if (snaps.connectionState == ConnectionState.done){
-                  if (snaps.hasError){
-                    text = Text('Error: ${snaps.error}');
-                  }else if (snaps.hasData){
-                    text = Text('Path: ${snaps.data.path}');
-                  }else{
-                    text = Text("Unavailable");
-                  }
-                }
-                return new Container(
-                  child: text,
-                );
+              child: Text("Open Link"),
+              onPressed: (){
+                launchURL(URL);
               },
             ),
           ],
         ),
-      ),
+      ) ,
     );
-  }
-}
-
-
-class Storage{
-  Future<String> get localPath async{
-    final dir = await getApplicationDocumentsDirectory();
-    return dir.path;
-  }
-  Future<File> get localFile async{
-    final path = await localPath;
-    return File('$path/db.txt');
-  }
-
-  Future<String> readData() async{
-    try {
-      final file = await localFile;
-      String body = await file.readAsString();
-      return body;
-    }catch (e){
-      return e.toString();
-    }
-  }
-  Future<File> writeData(String data) async{
-    final file = await localFile;
-    return file.writeAsString('$data');
   }
 }
